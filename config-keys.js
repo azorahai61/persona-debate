dd89416baef65d25fce137861a3ddb8f3544d9e9
azorahai61 <maheshwariyash61@gmail.com>
2026-02-24 20:11:25 -0800

    Demo only
    Demo only: intentionally commit exposed API key for Render static deploy

---
+++ config-keys.js
@@ -0,0 +1,5 @@
+// This file contains your API key
+// It is gitignored and will NOT be pushed to GitHub
+const CONFIG_KEYS = {
+  OPENAI_API_KEY: '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'
+};
